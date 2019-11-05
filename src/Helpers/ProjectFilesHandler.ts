a10b85ccae4420a6946de4d62540750fecec0a1e
DaceKonn <dacekonn@gmail.com>
2019-11-05 08:34:45 +0100

    wip

--- src/Helpers/ProjectFilesHandler.ts
+++ src/Helpers/ProjectFilesHandler.ts
@@ -1,14 +1,19 @@
 import * as fs from 'fs';
 import { getProjectFolders } from '../Models/ProjectFolders';
+import { workspace } from 'vscode';
 
 export class ProjectFilesHandler {
 
     public initProjectStructure() {
         
         var folders = getProjectFolders();
+        var workspaceFolders: string[] = [];
+        if (workspace.workspaceFolders !== undefined) {
+            workspaceFolders = workspace.workspaceFolders.map(value => value.name);
+        }
 
         folders.forEach(function (element) {
-            console.log('Folder exists - ' + element + ' - '+ fs.existsSync(element));
+            console.log(element + ' ' + workspaceFolders.includes(element));
         });
     }
 }
--- src/Models/ProjectFolders.ts
+++ src/Models/ProjectFolders.ts
@@ -1,6 +1,6 @@
-export const ManuscriptsFolder: string = '/Manuscripts/';
-export const ProjectBibleFolder: string = '/ProjectBible/';
-export const ProjectStatisticsFolders: string = '/ProjectStatistics/';
+export const ManuscriptsFolder: string = 'Manuscripts\\';
+export const ProjectBibleFolder: string = 'ProjectBible';
+export const ProjectStatisticsFolders: string = '.\\ProjectStatistics\\';
 
 export function getProjectFolders() {
     return [ManuscriptsFolder, ProjectBibleFolder, ProjectStatisticsFolders];
